a100e31b05301baad44a987644b61dab539c52a5
Yacine <126400076+yellow-alex@users.noreply.github.com>
2026-04-09 19:31:15 +0100

    finished the project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Rock-Paper-Scissors-
 I am on the verge of caving in i run back to the dark.
+No i don't want no scrub, a scrub is a guy that can't get no love from me.
--- rok.js
+++ rok.js
@@ -1,14 +1,53 @@
+let humScore = 0
+let compScore = 0 
+
 function getCompChoice(){
     let rando = Math.floor(Math.random() * 3 )  
     let RPS = ""
     if(rando == 0)
-        RPS = "Rock"
+        RPS = "rock"
     else if(rando == 1)
-        RPS = "Paper"
+        RPS = "paper"
     else
-        RPS = "Scissors"
+        RPS = "scissors"
     return RPS
 }
+
 function getHumChoice(){
-    let Pick = window.prompt("Rock paper or scissors")
+    let Pick = window.prompt("Rock paper or scissors").toLowerCase()
+    return Pick
 }
+
+function playRound(Human, Computer){
+    if(Human == Computer){
+        alert(`you both picked ${Human} you both got a point.`)
+        compScore++
+        humScore++
+    }
+    else if((Human == "rock" && Computer == "paper") || (Human == "scissors" && Computer == "rock") || (Human == "paper" && Computer == "scissors")){
+        alert(`On no! ${Human} loses to ${Computer}, Computer gets one point.`)
+        compScore++
+    }
+    else{
+        alert(`Good job! ${Human} beats ${Computer}, you get one point.`)
+        humScore++
+    }
+}
+function playGame(){
+    let hum = getHumChoice()
+    let comp = getCompChoice()
+    playRound(hum, comp)
+}
+
+playGame()
+playGame()
+playGame()
+playGame()
+playGame()
+
+if(humScore > compScore)
+    alert('ggs you Win!')
+else if(compScore > humScore)
+    alert('lmfao you lost, loser!')
+else
+    alert('What the fuck? how you picked both the same one five tines in a row ?')
